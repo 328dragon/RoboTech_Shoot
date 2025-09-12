9cd472036126d88e0dc85238b2dbf67e51799502
dragon <3516197926@qq.com>
2025-09-12 13:59:51 +0800

    修改车身长度，但是yaw偏了有点多，应该更新控制版本

--- MDK-ARM/Hardware/map.cpp
+++ MDK-ARM/Hardware/map.cpp
@@ -3,7 +3,7 @@ using namespace Map;
 
 namespace Map
 {
-    MapInfo_t MapLeft={{1.6,-0.4,0}};
-    MapInfo_t MapMide={{1.6,-1.4,0}};
-    MapInfo_t MapRight={{1.6,-2.4,0}};
+    MapInfo_t MapLeft={{1.6,-0.43,0}};
+    MapInfo_t MapMide={{1.6,-1.43,0}};
+    MapInfo_t MapRight={{1.6,-2.43,0}};
 } // namespace Map
